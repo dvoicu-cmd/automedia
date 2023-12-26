c8a684bd80b79c54eee55bc58171faa3cfcdfb54
dvoicu <dvoicu00@gmail.com>
2023-12-26 13:41:14 -0500

    Began work on service creation files.
    Currently implimented service_configurator.py to handle configuration files

---
+++ src/lib/create_service/create_service.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+import subprocess
+import configparser
+
+
+class CreateService:
+
+    def __init__(self, service_name, python_file):
+        """
+        Constructor
+        Args:
+            service_name (str): The name of the service
+            python_file (str): The name of the
+        """
+        self.service_name = service_name
+        self.python_file = python_file
+
+
