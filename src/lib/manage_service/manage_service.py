9eab8c2b15bac18ee25b7ff3b088912bbb128654
dvoicu <dvoicu00@gmail.com>
2023-12-30 16:35:07 -0500

    Worked on timer map class and manage service class
    I have yet to test the implimentations

--- src/lib/manage_service/manage_service.py
+++ src/lib/manage_service/manage_service.py
@@ -8,44 +8,51 @@ ex: OnUnitActiveSec=5min activates the script every five minutes
 """
 
 import subprocess
+import os
 from .service_configurator import ServiceConfigurator
+from .timer_map import TimerMap
 
 
 class ManageService:
-    def __init__(self, service_name, python_file):
+    def __init__(self):
         """
         Constructor
         Args:
-            service_name (str): The name of the service
-            python_file (str): The name of the
+            python_file (str): The file name of the python script that will be run as a service
         """
-        self.service_name = service_name
         self.service_config = ServiceConfigurator()
+        self.timer_map = TimerMap()
 
-    def write_path_config(self, exec_start='', working_directory='',  # Service file param
-                          on_boot_sec='', on_unit_active_sec='', on_calendar='',  # Timer file param
-                          abs_file_path=''):  # Location param
-        """
+    def create(self, python_file, on_calendar_list):
+        # Load the saved timer map
+        self.timer_map.deserialize()
 
-        :param exec_start:
-        :param working_directory:
-        :param on_boot_sec:
-        :param on_unit_active_sec:
-        :param on_calendar:
-        :param abs_file_path:
-        :return:
-        """
-        self.service_config.write(exec_start, working_directory, on_boot_sec, on_unit_active_sec, on_calendar, abs_file_path)
+        # Attempt to add the file name as a key
+        self.timer_map.new_timer_key(python_file)
 
-    def create(self):
+        # Add on_calendar_list to the mapping
+        self.timer_map.new_exec_times(python_file, on_calendar_list)
+
+        # If the exec times are empty after adding
+        exec_times = self.timer_map.get_exec_times(python_file)
+        if exec_times is None:
+            raise ValueError(f"The provided list: {on_calendar_list} conflicts with all the currently scheduled timers")
+
         # Write the service file
+        self.__write_service_file(python_file)
 
         # Write the timer file
+        self.__write_timer_file(python_file, exec_times)
 
         # Activate the service with subsystem
+        self.__activate_service(python_file)
+
+        # Save serialize the timer map
         return
 
-    def delete(self):
+    def delete(self, python_file):
+        # Load the saved timer map
+
         # Identify service file and timer file
 
         # Stop services with subsystem
@@ -54,34 +61,103 @@ class ManageService:
 
         return
 
-    def __activate_service(self):
+    def create_paths_config(self, service_dir_path, python_runtime_path, python_scripts_path):
+        """
+
+        :param service_dir_path:
+        :param python_runtime_path:
+        :param python_scripts_path:
+        :return:
+        """
+        self.service_config.write(service_dir_path, python_runtime_path, python_scripts_path)
+
+    def __activate_service(self, py_file):
         """
         Activates the service on the host computer
         :return:
         """
+        # Enable the service file
+
+        # Enable the timer file
+
+        # Then start the timer file
+
         return
 
-    def __stop_service(self):
+    def __stop_service(self, py_file):
         """
 
         :return:
         """
+        # Stop the timer file
+
+        # Disable the timer file
+
+        # Disable the service file
+
+
         return
 
-    def __write_service_file(self):
+    def __write_service_file(self, py_file):
         """
         Writes the .service file at the specified location
         :return:
         """
-        return
+        path_dict = self.service_config.read()
 
-    def __write_timer_file(self):
+        file_content = (f"[Unit]\n"
+                        f"Description=''\n"
+                        f"After=multi-user.target\n"
+                        f"Conflicts=getty@tty1.service\n"
+                        f"Wants={py_file}.timer\n"
+                        f"\n"
+                        f"[Service]\n"
+                        f"Type=simple\n"
+                        f"User=root\n"
+                        f"ExecStart={path_dict.get('python_runtime_path')} {path_dict.get('python_scripts_path')}/{py_file}.py\n"
+                        f"WorkingDirectory={path_dict.get('python_scripts_path')}\n"
+                        f"KillMode=process\n"
+                        f"\n"
+                        f"[Install]\n"
+                        f"WantedBy=multi-user.target")
+
+        # write file in service directory
+        with open(f"{path_dict.get('service_dir_path')}/{py_file}.service", 'w') as f:
+            f.write(file_content)
+
+    def __write_timer_file(self, py_file, on_calendar_list):
         """
         Writes the .timer file at the specified location
         :return:
         """
-        return
+        path_dict = self.service_config.read()
 
+        file_content = (f"[Unit]\n"
+                        f"Description=''\n"
+                        f"Requires{py_file}.service\n"
+                        f"\n"
+                        f"[Timer]\n"
+                        f"Unit={py_file}.service")
+
+        # Append the on calendar elements
+        for on_calendar_element in on_calendar_list:
+            file_content = f"{file_content}\nOnCalendar={on_calendar_element}\n"
+
+        # Add remaining items to file
+        file_content = (f"{file_content}"
+                        f"[Install]\n"
+                        f"WantedBy=timers.target")
+
+        with open(f"{path_dict.get('service_dir_path')}/{py_file}.timer", 'w') as f:
+            f.write(file_content)
+
+    def __delete_service_file(self, py_file):
+        path_dict = self.service_config.read()
+        os.remove(f"{path_dict.get('service_dir_path')}/{py_file}.service")
+
+    def __delete_timer_file(self, py_file):
+        path_dict = self.service_config.read()
+        os.remove(f"{path_dict.get('service_dir_path')}/{py_file}.timer")
 
 
 """
--- src/lib/manage_service/timer_map.py
+++ src/lib/manage_service/timer_map.py
@@ -1,5 +1,6 @@
 import pickle
 import re
+import json
 
 
 class TimerMap:
@@ -16,6 +17,7 @@ class TimerMap:
             self.data = loaded_data
 
     def new_timer_key(self, timer_name):
+        self.__verify_key(timer_name)
         self.data[timer_name] = []
 
     def delete_timer_key(self, timer_name):
@@ -23,30 +25,47 @@ class TimerMap:
         del self.data[timer_name]
 
     def new_exec_times(self, timer_name, on_calendar_values):
+        """
+        Adds a new on_calendar value to a timer
+        :param timer_name: name of the timer
+        :param on_calendar_values: list of stings in the format: DOW YYYY-MM-DD HH:MM:SS
+        """
         self.__verify_key(timer_name)
+
+        all_values = set(self.data.values())
+
         for on_calendar_entry in on_calendar_values:
             # First verify format
             if self.__is_on_calendar_format(on_calendar_entry):
-                # Then append the value
-                self.data[timer_name].append(on_calendar_entry)
+                # Add only if this does not conflict with any other scheduled times
+                if on_calendar_entry not in all_values:
+                    self.data[timer_name].append(on_calendar_entry)
 
     def get_exec_times(self, timer_name):
         self.__verify_key(timer_name)
         return self.data.get(timer_name)
 
-    def __is_on_calendar_format(self, str):
+    def json_return(self):
+        return json.dumps(self.data, indent=4)
+
+    def __is_on_calendar_format(self, str_input):
         pattern = r'^(\*|\d{1,4}|\w{3}|\d{4}-\d{2}-\d{2}|\d{2}:\d{2}:\d{2}|\~\d*|\d*\/\d+|\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})$'
         # Explanation of the pattern:
         # ^ - Start of string
         # (...)$ - Enclose the entire pattern and specify it should match the entire string
 
         # Use re.match to check if the string matches the pattern
-        if re.match(pattern, str):
+        if re.match(pattern, str_input):
             return True
         else:
             return False
 
     def __verify_key(self, key):
+        """
+        Checks to see if a key input is valid. If not, raise a value error
+        :param key:
+        :return:
+        """
         if self.data.get(key) is None:
-            raise ValueError(f"{key} is not a timer")
+            raise ValueError(f"{key} is not a timer/does not exists")
 
