da7b8027b91510434ef528ac9f77ad65cf801c43
dvoicu <dvoicu00@gmail.com>
2024-01-15 14:45:55 -0500

    Implimented yt_uploader script

--- src/publisher/platform_strategies/yt_upload.py
+++ src/publisher/platform_strategies/yt_upload.py
@@ -1,10 +1,11 @@
-import configparser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import os
+
+import selenium.common.exceptions
+import urllib3.exceptions
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
-
-import pdb
 
 
 class YtUpload:
@@ -17,39 +18,89 @@ class YtUpload:
         self.__login_google(email, password)
         self.title = "default"
         self.description = "default"
-        self.tags = "default, default" # or a list. Comma seperated, figure that out.
-
-    def load_config(self):
-        """
-        Loads the configuration
-        """
-        print("loads the config")
-
-    def set_upload(self, content_path):
-        """
-        Sets the content of the upload
-        Args:
-            content_path (str): The directory path of the content
-        """
+        self.tags = "default1,default2,default3"
+        self.for_kids = False
+        self.paid_promo = False
+        self.thumbnail_config = {"set_thumbnail": False, "path": None}
 
     def set_title(self, title):
         """
         Sets the title of the upload
         Args:
-            title (str): a string less than 100 char
+            title (str): a string less than 100 characters.
         """
+        # title has 100-character limit
+        if not len(title) > 100:
+            self.title = title
 
     def set_description(self, description):
         """
         Sets the description of the upload
         Args:
-            description (str): a string less than 5000 char.
+            description (str): a string less than 5000 characters.
         """
+        # 5000 character limit
+        if not len(description) > 5000:
+            self.description = description
+
+    def set_tags(self, tags):
+        """
+        Sets the tags for an upload
+        Args:
+            tags (str): A string with the words separated by commas. For example: tag1,tag2,tag3, ... tagN.
+            Input can't be over 500 characters (including commas).
+        """
+        # character count 500 (including commas)
+        if not len(tags) > 500:
+            self.tags = tags
+
+    def toggle_child_content(self):
+        """
+        Toggles the content to be for or not for kids
+        """
+        if self.for_kids:
+            self.for_kids = False
+        else:
+            self.for_kids = True
+
+    def toggle_paid_promotions(self):
+        """
+        Toggles the content to state if it has paid promotions
+        """
+        if self.paid_promo:
+            self.paid_promo = False
+        else:
+            self.paid_promo = True
+
+    def enable_thumbnail(self, img_path):
+        """
+        Configures to set a thumbnail. Thumbnail file can't be over 2mb
+        """
+        if os.path.exists(img_path):
+            # Get the size of the file in bytes
+            file_size_in_bytes = os.path.getsize(img_path)
+
+            # Convert bytes to megabytes
+            file_size_in_mb = file_size_in_bytes / (1024 * 1024)
+
+            if file_size_in_mb <= 2:
+                self.thumbnail_config["set_thumbnail"] = True
+                self.thumbnail_config["path"] = img_path
+
+    def disable_thumbnail(self):
+        """
+        Configures to not upload a thumbnail
+        """
+        self.thumbnail_config["set_thumbnail"] = False
+        self.thumbnail_config["path"] = None
 
     def exec_upload(self, file_path):
         """
         Executes the upload process
         """
+        if not os.path.exists(file_path):
+            raise ValueError(f"Video path does not exists: {file_path}")
+
         self.driver.implicitly_wait(time_to_wait=1)
 
         # Click create button
@@ -64,40 +115,43 @@ class YtUpload:
         # Send file to upload button
         upload_button.send_keys(file_path)
 
-        # Add expected condition, wait for it to load
+        # Wait for next button to be seen
+        # WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[2]/div/div[2]/ytcp-button[2]')))
+        self.driver.implicitly_wait(time_to_wait=10)
 
-        # Wait for the upload animation to play (usually takes 4 secs)
-        self.driver.implicitly_wait(time_to_wait=8)
-        # EC expected condition wait for next button to apear here
-
-        # Set the title 100-character limit
-
+        # Set the title
         title_input = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[1]/ytcp-video-title/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div')
         title_input.clear()
         title_input.send_keys(self.title)
 
-        # Set the Description 5000 character limit
+        # Set the description
         desc_input = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[2]/ytcp-video-description/div/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div')
         desc_input.clear()
         desc_input.send_keys(self.description)
 
-        # Set thumbnail
-        # First verify the number on an account
+        # Set thumbnail if configured to do so
+        if self.thumbnail_config["set_thumbnail"]:
+            thumbnail_input = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[3]/ytcp-thumbnails-compact-editor/div[3]/ytcp-thumbnails-compact-editor-uploader/ytcp-thumbnail-uploader/input')
+            thumbnail_input.send_keys(self.thumbnail_config["path"])
 
         # Set not for kids
-        not_for_kids = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[5]/ytkc-made-for-kids-select/div[4]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[2]/div[1]')
-        not_for_kids.click()
+        if self.for_kids:
+            for_kids = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[5]/ytkc-made-for-kids-select/div[4]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[1]/div[1]')
+            for_kids.click()
+        else:
+            not_for_kids = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[5]/ytkc-made-for-kids-select/div[4]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[2]/div[1]')
+            not_for_kids.click()
 
         # Toggle advanced settings
         show_more = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/div/ytcp-button')
         show_more.click()
 
-        # Enable paid promotions
-        toggle_paid_promo = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-advanced/div[1]/ytcp-checkbox-lit/div[1]')
-        toggle_paid_promo.click()
+        # Enable paid promotions if configured to do so.
+        if self.paid_promo:
+            toggle_paid_promo = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-advanced/div[1]/ytcp-checkbox-lit/div[1]')
+            toggle_paid_promo.click()
 
-        # Input tags, Character count 500 (including commas)
-        # err
+        # Input tags,
         tags_input = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-advanced/div[5]/ytcp-form-input-container/div[1]/div/ytcp-free-text-chip-bar/ytcp-chip-bar/div/input')
         tags_input.clear()
         tags_input.send_keys(self.tags)
@@ -122,22 +176,19 @@ class YtUpload:
         # Click the publish button
         publish_button = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[2]/div/div[2]/ytcp-button[3]')
         publish_button.click()
-        self.driver.implicitly_wait(time_to_wait=5)
+        self.driver.implicitly_wait(time_to_wait=15)
 
-        # Close the final pop up
-        close_button = self.driver.find_element(By.XPATH, '/html/body/ytcp-video-share-dialog/ytcp-dialog/tp-yt-paper-dialog/div[3]/ytcp-button')
-        close_button.click()
-
+        self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-still-processing-dialog/ytcp-dialog/tp-yt-paper-dialog/div[3]/ytcp-button').click()
 
     def select_account(self, account_name):
         # Get to the list of the associated accounts.
-        self.driver.implicitly_wait(time_to_wait=3)
+        self.driver.implicitly_wait(time_to_wait=5)
         account_btn = self.driver.find_element(By.XPATH, '/html/body/ytcp-app/ytcp-entity-page/div/ytcp-header/header/div/ytd-topbar-menu-button-renderer/button')
         account_btn.click()
-        self.driver.implicitly_wait(time_to_wait=1)
+        self.driver.implicitly_wait(time_to_wait=5)
         switch_account = self.driver.find_element(By.XPATH, '/html/body/ytcp-app/ytcp-popup-container/tp-yt-iron-dropdown/div/ytd-multi-page-menu-renderer/div[3]/div[1]/yt-multi-page-menu-section-renderer[1]/div[2]/ytd-compact-link-renderer[3]/a/tp-yt-paper-item')
         switch_account.click()
-        self.driver.implicitly_wait(time_to_wait=1)
+        self.driver.implicitly_wait(time_to_wait=5)
         div_list_accounts = self.driver.find_element(By.XPATH, '/html/body/ytcp-app/ytcp-popup-container/tp-yt-iron-dropdown/div/ytd-multi-page-menu-renderer/div[4]/ytd-multi-page-menu-renderer/div[3]/div[1]/ytd-account-section-list-renderer/div[2]/ytd-account-item-section-renderer/div[2]')
 
         # Grab the list of webelements containing the accounts
--- test/publisher_test/yt_upload.py
+++ test/publisher_test/yt_upload.py
@@ -1,11 +1,18 @@
 from context import src
 
 from src.publisher.platform_strategies.yt_upload import YtUpload
+import pdb
 
 def test_login():
-    yt = YtUpload("dvmedia00@gmail.com", "") # you thought
-    yt.select_account("arghhh")
+    yt = YtUpload("dvmedia00@gmail.com", "")
+    yt.select_account("heeheehaw")
+    yt.enable_thumbnail("/Users/dvoicu/mnt/Goofy Aughhh Test Files copy/Lyra IV.png")
+    yt.set_title("Fake")
+    yt.set_tags("Not,okay,delete,soon")
+    yt.set_description("2mls of water will do the trick")
+    yt.toggle_paid_promotions()
     yt.exec_upload("/Users/dvoicu/When2HrOfSleep.mp4")
+    pdb.set_trace()
 
 
 test_login()
