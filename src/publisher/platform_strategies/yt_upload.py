9bd9abf51e4286740cf037497f2dc5c85a82d4b1
dvoicu <dvoicu00@gmail.com>
2024-01-15 01:48:14 -0500

    Implimented auto upload script. LETS GO!!!

--- src/publisher/platform_strategies/yt_upload.py
+++ src/publisher/platform_strategies/yt_upload.py
@@ -15,6 +15,9 @@ class YtUpload:
         """
         self.driver = uc.Chrome()
         self.__login_google(email, password)
+        self.title = "default"
+        self.description = "default"
+        self.tags = "default, default" # or a list. Comma seperated, figure that out.
 
     def load_config(self):
         """
@@ -43,19 +46,87 @@ class YtUpload:
             description (str): a string less than 5000 char.
         """
 
-    def exec_upload(self):
+    def exec_upload(self, file_path):
         """
         Executes the upload process
         """
+        self.driver.implicitly_wait(time_to_wait=1)
 
-        # --- First set up the webdriver ---
-        options = Options()
-        if(True): # If headless is configured, add headless option
-            options.add_argument('--headless')
-        driver = webdriver.Chrome(options=options)
+        # Click create button
+        self.driver.find_element(By.ID, 'create-icon').click()
 
-        # Set up link
-        driver.get("studio.youtube.com")
+        # Click upload video
+        self.driver.find_element(By.ID, 'text-item-0').click()
+
+        # Get the upload button
+        upload_button = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-uploads-file-picker/div/input')
+
+        # Send file to upload button
+        upload_button.send_keys(file_path)
+
+        # Add expected condition, wait for it to load
+
+        # Wait for the upload animation to play (usually takes 4 secs)
+        self.driver.implicitly_wait(time_to_wait=8)
+        # EC expected condition wait for next button to apear here
+
+        # Set the title 100-character limit
+
+        title_input = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[1]/ytcp-video-title/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div')
+        title_input.clear()
+        title_input.send_keys(self.title)
+
+        # Set the Description 5000 character limit
+        desc_input = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[2]/ytcp-video-description/div/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div')
+        desc_input.clear()
+        desc_input.send_keys(self.description)
+
+        # Set thumbnail
+        # First verify the number on an account
+
+        # Set not for kids
+        not_for_kids = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[5]/ytkc-made-for-kids-select/div[4]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[2]/div[1]')
+        not_for_kids.click()
+
+        # Toggle advanced settings
+        show_more = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/div/ytcp-button')
+        show_more.click()
+
+        # Enable paid promotions
+        toggle_paid_promo = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-advanced/div[1]/ytcp-checkbox-lit/div[1]')
+        toggle_paid_promo.click()
+
+        # Input tags, Character count 500 (including commas)
+        # err
+        tags_input = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-advanced/div[5]/ytcp-form-input-container/div[1]/div/ytcp-free-text-chip-bar/ytcp-chip-bar/div/input')
+        tags_input.clear()
+        tags_input.send_keys(self.tags)
+
+        # get the next button and click it to move to next page.
+        next_button = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[2]/div/div[2]/ytcp-button[2]')
+        next_button.click()
+        self.driver.implicitly_wait(time_to_wait=5)
+
+        # Skip video elements
+        next_button.click()
+        self.driver.implicitly_wait(time_to_wait=5)
+        # Skip checks
+        next_button.click()
+        self.driver.implicitly_wait(time_to_wait=5)
+
+        # Now click visibility to public
+        public_button = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-uploads-review/div[2]/div[1]/ytcp-video-visibility-select/div[2]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[3]')
+        public_button.click()
+        self.driver.implicitly_wait(time_to_wait=5)
+
+        # Click the publish button
+        publish_button = self.driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[2]/div/div[2]/ytcp-button[3]')
+        publish_button.click()
+        self.driver.implicitly_wait(time_to_wait=5)
+
+        # Close the final pop up
+        close_button = self.driver.find_element(By.XPATH, '/html/body/ytcp-video-share-dialog/ytcp-dialog/tp-yt-paper-dialog/div[3]/ytcp-button')
+        close_button.click()
 
 
     def select_account(self, account_name):
@@ -83,8 +154,6 @@ class YtUpload:
 
         # Then switch to that account
         account_to_switch_to.click()
-
-        pdb.set_trace()
 
 
     def __login_google(self, account, password):
--- test/publisher_test/yt_upload.py
+++ test/publisher_test/yt_upload.py
@@ -3,8 +3,9 @@ from context import src
 from src.publisher.platform_strategies.yt_upload import YtUpload
 
 def test_login():
-    yt = YtUpload("dvmedia00@gmail.com", "")
+    yt = YtUpload("dvmedia00@gmail.com", "") # you thought
     yt.select_account("arghhh")
+    yt.exec_upload("/Users/dvoicu/When2HrOfSleep.mp4")
 
 
 test_login()
