fd750c50d064bdc0575140790c3dc154f5d279bd
dvoicu <dvoicu00@gmail.com>
2024-01-19 16:51:41 -0500

    Testing multiple voices selector.

--- src/scraper/platform_strategies/pyttsx3_wraper.py
+++ src/scraper/platform_strategies/pyttsx3_wraper.py
@@ -4,6 +4,12 @@ from src.scraper.downloader import DownloadManager
 
 class TTS:
     def __init__(self, rate, volume, gender):
+        """
+
+        :param rate:
+        :param volume:
+        :param gender:
+        """
         self.engine = pyttsx3.init()
         self.dl = DownloadManager()
 
@@ -11,5 +17,21 @@ class TTS:
         self.engine.setProperty('volume', volume)  # [0,1] range
 
     def dl_text(self, text, file_name):
+        """
+        Works only on linux distros.
+        :param text:
+        :param file_name:
+        :return:
+        """
         self.engine.save_to_file(text, f'{self.dl.dl_root()}/audio/{self.dl.get_rand_id()}_{file_name}.mp3')
+        self.engine.runAndWait()
+
+    def say_text(self, text):
+        voices = self.engine.getProperty('voices')
+        for voice in voices:
+            self.engine.setProperty('voice', voice.id)
+            self.engine.say('The quick brown fox jumped over the lazy dog.')
+        self.engine.runAndWait()
+
+        self.engine.say(text, "say_text")
         self.engine.runAndWait()
--- test/scraper_tests/tts_wrap_test.py
+++ test/scraper_tests/tts_wrap_test.py
@@ -2,5 +2,5 @@ from context import src
 from src.scraper.platform_strategies.pyttsx3_wraper import TTS
 
 tts = TTS(125,0.5,'g')
-tts.dl_text('Sample text, bottom text. Real shit right here am I right?', 'real')
+tts.say_text('Sample text, bottom text. Real shit right here am I right?')
 
