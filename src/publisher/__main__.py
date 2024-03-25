09871a9350b98f59162200c983ab0c9c6435d0f7
dvoicu <dvoicu00@gmail.com>
2024-03-25 00:10:07 -0400

    Implimented formulas for scraper and publisher.

--- src/publisher/__main__.py
+++ src/publisher/__main__.py
@@ -12,8 +12,47 @@ def main():
     if v1 == 'custom':
 
         # CUSTOM PUBLISHER FORMULA CREATION
+        f = ManageFormula()
 
-        pass
+        service_name = InputPage("Input the name of the service").prompt()
+        name = InputPage("Input the name of the account you want to set up a YT publisher for").prompt()
+
+        f.ap(f""""
+db = DbNasConnection()
+manager = PublisherDirManager()
+
+# 0 -> id, 1 -> username, 2 -> email, 3 -> password, 4 -> auth_secrete, 5 -> platform, 6 -> description
+acc_record = db.read_account_by_name('{name}')
+
+# 0 -> id, 1 -> file location, 3 -> title, 4 -> description, 5 -> to archive
+content_record = db.read_rand_content_file(acc_record[0])  # Reads the content.
+
+# Set up uploader
+yt = YtUpload(acc_record[3], acc_record[4], acc_record[5])
+yt.select_account(acc_record[1])
+
+# Set title
+yt.set_title(content_record[3])
+# Set description
+yt.set_description(content_record[4])
+
+# organize files
+file_path = content_record[1]  # Expected directory with video and thumbnail.
+        """)
+
+        f.ap("""
+# set thumbnail (if verified)
+yt.enable_thumbnail(f"{file_path}/thumbnail.jpg")
+
+# exec
+yt.exec_upload(f"{file_path}/video.mp4")
+
+# Set to archive
+db.update_to_archived('content_files', content_record[0])
+        """)
+
+        f.save_generated_script(service_name)
+        print(200)
 
     if v1 == 'manual':
 
--- src/scraper/__main__.py
+++ src/scraper/__main__.py
@@ -11,7 +11,38 @@ def main():
     v1 = pg.main_menu("Scraper")
     if v1 == 'custom':
         # Make scraper formula
-        pass
+        f = ManageFormula()
+
+        name = InputPage("Input the name of the service").prompt()
+        desc = InputPage("Input the description for the scrapes").prompt()
+        subreddit = InputPage("Input the subreddit name").prompt()
+        media_pool = InputPage("Input the corresponding media_pool").prompt()
+
+        f.ap(f"""
+manager = ScraperDirManager()
+tmp = manager.create_tmp_dir()
+
+db = DbNasConnection()
+
+scrapes = RedditScrape().scrape("{subreddit}", "hot", "text", 1, 5)
+        """)
+
+        # This is stupid, but it works.
+        s = f'manager.dl_list_of_text(scrapes, f"{subreddit}_'
+        s = s + '{manager.get_rand_id()}", tmp)'
+        f.ap(s)
+
+        f.ap(f"""
+files = manager.select_dir(tmp)
+
+for file in files:
+    db.create_media_file(file, "text", os.path.basename(file), "{desc}", "{media_pool}")
+
+manager.cleanup(tmp)
+        """)
+
+        f.save_generated_script(name)
+        print(200)
 
     if v1 == 'manual':
 
