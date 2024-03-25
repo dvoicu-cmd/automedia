240050fd68a0d6abf8cb8b4d6fb4245c5ab6dfff
dvoicu <dvoicu00@gmail.com>
2024-03-25 16:16:18 -0400

    Tried to make creator main more clear, and implimented option to archive media files.

--- src/creator/__main__.py
+++ src/creator/__main__.py
@@ -21,11 +21,13 @@ def main():
         service_name = InputPage("Give a title to the service:").prompt()
 
         f.ap("""
+        
 manager = CreatorDirManager()
 output_tmp = manager.create_tmp_dir("am_i_the_a_hole_upload")
 db = DbNasConnection()
 
 print("-> Created DbNas Connection")
+        
         """)
 
         # Pick the canvas
@@ -53,30 +55,57 @@ print("-> Created DbNas Connection")
         num_text_content = InputPage("Input the number of stories to load from the media pool").prompt()
 
         f.ap(f"""
-
+        
 media_pool_id = {text_content}
 num_story = {num_text_content}
 i = 1 
         """)
 
-# Reads in the stories
+        # Prompt to archive
+        archive = PickerPage(["Yes", "No"]).prompt("Do you wish to archive the text content after use?")
+
+        # Reads in the stories
         f.ap("""
-story_text = manager.read_text(db.nas_root() + "/" + db.read_rand_media_file_of_pool(media_pool_id)[1])
+        
+record = db.read_rand_media_file_of_pool(media_pool_id)
+story_text = manager.read_text(db.nas_root() + "/" + record[1])
+
+        """)
+
+        if archive == 0:
+            f.ap("""
+            
+db.update_to_archived("media_files", record[0])
+
+        """)
+
+        f.ap("""
+        
 while i <= num_story:
+    add_record = db.read_rand_media_file_of_pool(media_pool_id)
     story_text = story_text + " ... Next story. "
     story_text = story_text + manager.read_text(db.nas_root() + "/" + db.read_rand_media_file_of_pool(media_pool_id)[1])
     i = i + 1
+        
         """)
 
-# Call some tts
+        if archive == 0:
+            f.ap("""
+            
+    f.ap('db.update_to_archived("media_files", add_record[0])')
+            
+            """)
+
+        # Call some tts
         tts_name = InputPage("Give a tts voice: alloy, echo, fable, onyx, nova, or shimmer").prompt()
         f.ap(f"""
+        
 # Call a tts
 tts_tmp = manager.create_tmp_dir()
 story_narration = OpenAiAPI().text_to_speech("{tts_name}", story_text, tts_tmp)
 narration = AttachAudio(manager.select_dir_one(tts_tmp))
+        
         """)
-
 
         max_word_per_line = InputPage("SUBS: Input the max words per line").prompt()
         font = InputPage("SUBS: Input a valid font: \n Recommended: Arial-Bold").prompt()
@@ -84,7 +113,9 @@ narration = AttachAudio(manager.select_dir_one(tts_tmp))
         font_outline = InputPage("SUBS: Input size of font outline. \n Recommended: 4").prompt()
         whisper_model = InputPage("SUBS: Enter transcription accuracy: tiny, base, small, medium, large \n Recommended medium for optimal render time and accuracy").prompt()
 
+
         f.ap(f"""
+        
 # Create subtitles
 subs = AttachSubtitles(manager.select_dir_one(tts_tmp))
 txt = TextParam()
@@ -95,6 +126,7 @@ subs.set_text(txt)
 subs.set_whisper_model('{whisper_model}')
 subs.set_text_location(('center', 'center'))
 subs.set_max_word_per_line({max_word_per_line})
+        
         """)
 
 
@@ -121,19 +153,33 @@ subs.set_max_word_per_line({max_word_per_line})
         elif double_size == 1:
             pass
 
+        archive = PickerPage(["Yes", "No"]).prompt("Do you wish to archive the footage after use?")
+
         f.ap(f"""
+        
 # You need to count the length of the story narration as that is the edit that determines the duration.
 list_of_footage = []
 
 footage_duration_sum = 0
 while footage_duration_sum < narration.duration():
     # Find way to stretch video vertically wider
-    e = AttachMuteVideo(db.nas_root() + "/" + db.read_rand_media_file_of_pool({footage_id})[1], ('center', 'center'))  # get random parkour footage
+    record = db.read_rand_media_file_of_pool({footage_id})
+    e = AttachMuteVideo(db.nas_root() + "/" + record[1], ('center', 'center'))  # get random parkour footage
     e.set_start_and_end(footage_duration_sum, e.duration())  # set times
-    e.resize({width}, {height})  # 1920*2=3840 , 1080*2=2160
+    e.resize({width}, {height})
     list_of_footage.append(e)  # add the footage to the list
     footage_duration_sum += e.duration()  # add to sum
+    
+    """)
+        if archive == 0:
+            f.ap(f"""
+            
+    db.update_to_archived("media_files", record[0])
+    
+            """)
 
+        f.ap(f"""
+        
 print("-> Got Edits")
 
 # append the edits
@@ -147,25 +193,31 @@ print("-> Applying Edits")
 base.apply_edits(edits, narration)
 
 print("-> Applied, Rendering Video:")
+
         """)
 
         f.ap("""
+        
 # render footage to output dir
 base.render(f"{output_tmp}/video.mp4")
+
         """)
 
         make_thumb = PickerPage(["Yes", "No"]).prompt("Do you wish to create a thumbnail with your content? \n"
                                                       "This only applies to YT uploads")
         if make_thumb == 0:
-
             base_image = InputPage("Input the media pool with your base thumbnail image").prompt()
+            archive = PickerPage(["Yes", "No"]).prompt("Do you wish to archive the thumbnail after use?\n"
+                                                       "Ensure that you have a constant supply of thumbnail images if you do.")
 
             f.ap(f"""
+            
 print("-> Creating Thumbnail")
 
 # create thumbnail
 # 0 -> id, 1 -> file_location, 2 -> media_type, 3 -> title, 4 -> description, 5 -> to_archive
-img_location = db.nas_root() + "/" + db.read_specific_media_file_by_id({base_image})[1]
+record = db.read_rand_media_file_of_pool({base_image})
+img_location = db.nas_root() + "/" + record[1]
 
 canvas = SixteenByNine('1920x1080')
 thumb = MakeThumbnail(canvas=canvas)
@@ -182,21 +234,26 @@ ttxt.limit_words(16, 5)  # Each line can hold about 38 characters. average word 
 thumb.place_text(ttxt)
 
             """)
+
             f.ap('# Write thumbnail')
             f.ap('thumb.write(2, f"{output_tmp}", "thumbnail")')
+            if archive == 0:
+                f.ap('db.update_to_archived("media_files", record[0])')
+
         elif make_thumb == 1:
             f.ap("""
+            
 # Create post title (NO THUMBNAIL OPTION SELECTED)
 ttxt = ThumbnailText(story_text)
 ttxt.limit_words(16, 5)
+            
             """)
-            pass
-
 
         description = InputPage("Input a generic description that will be posted on all your videos?").prompt()
         account_name = InputPage("Input the associated account name this content will be uploaded to").prompt()
 
         f.ap(f"""
+        
 print("-> Uploading to DbNas")
 
 # upload to db nas
@@ -205,6 +262,7 @@ db.create_content(output_tmp, ttxt.text_content, "{description}", "{account_name
 # clean the tmp dirs
 manager.cleanup(output_tmp)
 manager.cleanup(tts_tmp)
+        
         """)
 
         # Save the script
