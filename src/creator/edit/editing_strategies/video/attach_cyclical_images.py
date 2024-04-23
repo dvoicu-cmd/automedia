dab84c20b92b350e6eb73b60c4e6ae1d564589b6
dvoicu <dvoicu00@gmail.com>
2024-04-23 19:34:20 -0400

    Added cyclical images class.

---
+++ src/creator/edit/editing_strategies/video/attach_cyclical_images.py
@@ -0,0 +1,33 @@
+from moviepy.editor import CompositeVideoClip, VideoFileClip, ImageSequenceClip
+from moviepy.video.fx.loop import loop
+from moviepy.video.fx.resize import resize
+from src.creator.edit.end_start_edit import EndStartEdit
+from lib.manage_directory_structure.dir_manager import DirManager
+
+
+class AttachCyclicalImages(EndStartEdit):
+
+    def __init__(self, source_dir: str, duration_between_img: int, location: tuple):
+        # Get the images
+        images = DirManager.select_dir(source_dir, file_filter="*.jpg")
+
+        # Then populate a list containing the duration between each img in sec.
+        duration_between = []
+        for i in range(len(images)):
+            duration_between.append(duration_between_img)
+
+        self.image_seq = ImageSequenceClip(images, durations=duration_between)
+        self.location = location
+
+    def apply(self, composite_clip: CompositeVideoClip) -> CompositeVideoClip:
+        # You need some sort of looping mechanism. Cuz rn it will just play the seq once and then it will be done.
+        self.image_seq = self.image_seq.set_position(self.location)  # apply location
+        output = CompositeVideoClip([composite_clip, self.image_seq])
+        return output
+
+    def duration(self) -> int:
+        return self.image_seq.duration
+
+    def set_start_and_end(self, start_time: int, end_time: int):
+        self.image_seq = self.image_seq.set_start(start_time)
+        self.image_seq = self.image_seq.set_end(end_time)
