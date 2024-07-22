5fe7af8477e2724abb3965c1529de5735b7514ef
dvoicu <dvoicu00@gmail.com>
2024-07-21 21:38:00 -0400

    Changed up manual local publishes to locally publish multiple files.

--- src/publisher/__main__.py
+++ src/publisher/__main__.py
@@ -23,44 +23,53 @@ def main():
 
     if v1 == 'manual':
 
-        v2 = PickerPage(["Random Content File", "Specific Content File"]).prompt("Manually Publishing a Content File.\nSelect an option.")
-        if v2 == 0:  # random content file
+        v2 = PickerPage(["Random Content Files",
+                         "Multiple Content Files",
+                         "Specific Content File",
+                         ]).prompt("Manually Publishing a Content File.\nSelect an option.")
+        if v2 == 0:  # random content files
             try:
                 account_id = InputPage("Publishing Random Content File.\nInput the account id").prompt()
+                num_media_exports = InputPage("Input the number of media files to export").prompt()
+
                 # init objs
                 lp = LocalPublish()
                 db_nas = DbNasConnection()
-
-                # get the record
-                record = db_nas.read_rand_content_file(account_id)
-
-                # get the file path for the content file
-                lp.set_src_path(record[1])
-
                 # set the destination to be just outside the project
-                cwd = os.getcwd()
-                cd_to_desired_root(cwd, "automedia")
-                os.chdir('..')
-                output_dir = os.getcwd()
-                os.chdir(cwd)
+                output_dir = export_dir()
 
-                # exec publish
-                lp.exec_upload(output_dir)
-
-                # archive
-                db_nas.update_to_archived('content_files', record[0])
+                i = 0
+                triggered_exception = False
+                stored_exception = ""
+                for i in range(num_media_exports):
+                    # get the record
+                    try:
+                        record = db_nas.read_rand_content_file(account_id)
+                    except Exception as e:
+                        triggered_exception = True
+                        stored_exception = e
+                        return
+                    # get the file path for the content file
+                    lp.set_src_path(record[1])
+                    # exec publish
+                    lp.exec_upload(output_dir)
+                    # archive
+                    db_nas.update_to_archived('content_files', record[0])
 
                 # Display success
-                DisplayPage().prompt("Published new content locally")
+                if triggered_exception:
+                    DisplayPage().prompt(
+                        f"Published {i} content file(s) locally. \n\n"
+                        f"An exception occurred while executing: {pg.str_exception(stored_exception)}"
+                    )
+                else:
+                    DisplayPage().prompt(f"Published {i} content file(s) locally")
 
             except Exception as e:
                 if isinstance(e, InputCancelled):
                     InputPage("").print_cancelled_input()
                 else:
                     DisplayPage().prompt(pg.str_exception(e))
-
-
-
 
         if v2 == 1:  # specific content file
             try:
@@ -85,12 +94,8 @@ def main():
                 # get the file path for the content file
                 lp.set_src_path(record[1])
 
-                # set the destination to be just outside the project
-                cwd = os.getcwd()
-                cd_to_desired_root(cwd, "automedia")
-                os.chdir('..')
-                output_dir = os.getcwd()
-                os.chdir(cwd)
+                # Get output dir
+                output_dir = export_dir()
 
                 # exec publish
                 lp.exec_upload(output_dir)
@@ -106,6 +111,28 @@ def main():
                 else:
                     DisplayPage().prompt(pg.str_exception(e))
 
+
+def export_dir():
+    """
+    Helper function for creating and returning the path of the local publishing exporting directory.
+    :return: Str path of the exporting directory.
+    """
+    # set the destination to be just outside the project
+    cwd = os.getcwd()
+    cd_to_desired_root(cwd, "automedia")
+    os.chdir('..')
+    outside_dir_path = os.getcwd()
+    output_dir = f"{outside_dir_path}/automedia_exports"
+
+    # make the exports directory if it does not exist
+    try:
+        os.mkdir(output_dir)
+    except FileExistsError:
+        pass
+
+    # Change back.
+    os.chdir(cwd)
+    return output_dir
 
 if __name__ == '__main__':
     main()
